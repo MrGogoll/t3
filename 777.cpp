5584b520aa6d4720a0e1a72c349d0b84ddc45024
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-10-23 16:43:55 +0300

    вывести все четные числа от a до b (включительно).

---
+++ 777.cpp
@@ -0,0 +1,16 @@
+﻿//вывести все четные числа от a до b (включительно). Числа вводятся с консоли.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, i;
+	cin >> a >> b;
+
+	for (int i = a; i <= b; i++) {
+		if (i % 2 == 0) {
+
+			cout << i << endl;
+
+		}
+	}
+}
