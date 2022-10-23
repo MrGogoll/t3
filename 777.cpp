8047f3b9838fc0d136000aff65b80c282234d313
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-10-23 16:55:50 +0300

    Арифметическая прогрессия

--- 777.cpp
+++ 777.cpp
@@ -1,16 +1,11 @@
-﻿//вывести все четные числа от a до b (включительно). Числа вводятся с консоли.
+﻿
 #include <iostream>
 using namespace std;
 int main()
 {
-	int a, b, i;
-	cin >> a >> b;
-
-	for (int i = a; i <= b; i++) {
-		if (i % 2 == 0) {
-
-			cout << i << endl;
-
-		}
-	}
+	int a, b, c;
+	cin >> a >> b >> c;
+	int sum;
+	sum = a + b + c;
+	cout << sum / 3;
 }
